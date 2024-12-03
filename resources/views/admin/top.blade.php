69be9cabef690d2e260d50666eb0f902862f3373
masayuki <masatajima12@gmail.com>
2024-12-03 20:56:34 +0900

    新規登録ボタンにonclick属性を付与

--- resources/views/admin/top.blade.php
+++ resources/views/admin/top.blade.php
@@ -8,7 +8,7 @@
     <div class="lg:w-1/2 md:w-2/3 mx-auto">
       <div class="flex flex-wrap -m-2">
         <div class="p-2 w-full">
-            <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            <button onclick="location.href='{{ route('admin.categories.create') }}'" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                 カテゴリー新規登録
             </button>
         </div>
