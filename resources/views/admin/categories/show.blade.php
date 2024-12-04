e43f87c1e2c75b574773de77025073b983631129
masayuki <masatajima12@gmail.com>
2024-12-04 21:19:34 +0900

    クイズ新規登録画面を作成

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -18,7 +18,7 @@
             <div class="sm:w-1/2 ml-auto mt-2">
                 <div class="flex flex-wrap -m-2">
                     <div class="p-2 w-full">
-                        <button onclick="location.href='{{ route('admin.categories.create') }}'" class="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
+                        <button onclick="location.href='{{ route('admin.categories.quizzes.create', ['categoryId' => $category->id]) }}'" class="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
                             クイズ新規登録
                         </button>
                     </div>
