3d6203ea5aa1ee3dea1e71cdc92789e0880d44bc
masayuki <masatajima12@gmail.com>
2024-12-04 20:31:00 +0900

    テーブル同士のリレーションを設定

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Option extends Model
 {
-    //
+    use HasFactory;
+
+    public function quiz()
+    {
+        return $this->belongsTo(Quiz::class);
+    }
 }
