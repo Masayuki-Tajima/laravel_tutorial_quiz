291be327bafe321738c46224beb66541d99530ad
masayuki <masatajima12@gmail.com>
2024-12-05 22:16:17 +0900

    クイズのスタート画面を作成

--- app/Http/Controllers/PlayController.php
+++ app/Http/Controllers/PlayController.php
@@ -7,10 +7,23 @@ use App\Models\Category;
 
 class PlayController extends Controller
 {
-    public function top(){
+    //プレイ画面トップページ
+    public function top()
+    {
         $categories = Category::all();
         return view('play.top', [
             'categories' => $categories,
+        ]);
+    }
+
+    //クイズスタート画面
+    public function categories(Request $request, int $categoryId)
+    {
+        $category = Category::withCount('quizzes')->findOrFail($categoryId);
+        // dd($category->quizzes_count);
+        return view('play.start', [
+            'category'     => $category,
+            'quizzesCount' => $category->quizzes_count,
         ]);
     }
 }
