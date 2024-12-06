a9114f71ab7eeef01944676de7cd31e33df351e9
masayuki <masatajima12@gmail.com>
2024-12-06 22:49:18 +0900

    正解・不正解の判定処理を作成

--- app/Http/Controllers/PlayController.php
+++ app/Http/Controllers/PlayController.php
@@ -45,10 +45,43 @@ class PlayController extends Controller
     //クイズ解答画面表示
     public function answer(Request $request, int $categoryId)
     {
-        // dd($categoryId, $request);
         $quizId = $request->quizId;
-        $optionId = $request->optionId;
+        $selectedOptions = $request->optionId;
 
+        $category = Category::with('quizzes.options')->findOrFail($categoryId);
+        $quiz = $category->quizzes->firstWhere('id', $quizId);
+        $quizOptions = $quiz->options->toArray();
+        $result = $this->isCorrectAnswer($selectedOptions, $quizOptions);
+        dd($result);
         return view('play.answer');
+    }
+
+    //プレイヤーの解答が正解か不正解かを判定
+    private function isCorrectAnswer(array $selectedOptions, array $quizOptions)
+    {
+        //クイズの選択肢から正解の選択肢を抽出し、そのIdをすべて取得する
+        $correctOptions = array_filter($quizOptions, function ($option) {
+            return $option['is_correct'] === 1;
+        });
+
+        //idの数字だけを抽出する
+        $correctOptionIds = array_map(function($option){
+            return $option['id'];
+        }, $correctOptions);
+
+        //プレイヤーが選んだ選択肢の個数と、正解の選択肢の個数が一致するかを判定する
+        if (count($selectedOptions) !== count($correctOptionIds)) {
+            return false;
+        }
+
+        //プレイヤーが選んだ選択肢のidと正解のidがすべて一致するか判定する
+        foreach ($selectedOptions as $selectedOption) {
+            if (!in_array((int)$selectedOption, $correctOptionIds)) {
+                return false;
+            }
+        }
+
+        //正解であることを返す
+        return true;
     }
 }
